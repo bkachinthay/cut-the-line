d0c1b1c7c9bda1fe2be58de0ae252e20532c60c5
bkachinthay <bkachinthay>
2020-04-17 07:42:42 +0530

    reduce token no font size for larger numbers

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,10 +1,14 @@
 import zpad from "utils/zpad";
 
-function Token({ value }) {
+function Token({ value = 0 }) {
   return (
     <div class="inline-flex flex-column justify-start">
       <span class="db f7 tc b blue">Token no</span>
-      <span class="db br-100 pa0 bw2 ba h3 w3 b--blue flex justify-center items-center black f1 lh-solid">
+      <span
+        class={`db br-100 pa0 bw2 ba h3 w3 b--blue flex justify-center items-center black ${
+          value < 100 ? "f1" : value < 1000 ? "f2" : "f3"
+        } lh-solid`}
+      >
         {zpad(value)}
       </span>
     </div>
