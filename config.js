0fe660597eb60e03c17e9a527dc76c021fcd251b
Bharath Kachinthay <bharathkachinthay@gmail.com>
2021-09-18 08:14:23 +0530

    add empty db directory

--- config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  pusherKey: "c5ea56ca0a83c80d0655",
-  pusherCluster: "ap2",
-  gqlURL: "https://graphql.fauna.com/graphql",
-};
--- config.sample.js
+++ config.sample.js
@@ -2,4 +2,5 @@ export default {
   pusherKey: "",
   pusherCluster: "",
   gqlURL: "",
+  apiEndpoint: ""
 };
